bd5bb4e921ce15310ce3a5750e7f52be8b0a344d
Haris007-M <azan6743@gmail.com>
2024-01-17 22:06:50 +0500

    added to the git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ButtonCarousel from "./component/example";
+
+import electronics from "./assets/electronics.svg";
+import sports from "./assets/sports.svg";
+import outdoor from "./assets/outdoor.svg";
+import toys from "./assets/toys.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "40px",
+        margin: "auto",
+      }}>
+      <ButtonCarousel />
+      {/* <nav className="p-5 bg-slate-500 rounded-sm">
+        <button
+          type="button"
+          class="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF]">
+          <img src={electronics} alt="" width={15} className="me-2" />
+          Electronics
+        </button>
+        <button
+          type="button"
+          class="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF]">
+          <img src={toys} alt="" width={15} className="me-2" />
+          Toys and Games
+        </button>
+        <button
+          type="button"
+          class="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF]">
+          <img src={sports} alt="" width={15} className="me-2" />
+          Sports
+        </button>
+        <button
+          type="button"
+          class="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF]">
+          <img src={outdoor} alt="" width={15} className="me-2" />
+          Outdoor living
+        </button>
+        <button
+          type="button"
+          class="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF]">
+          <img src={electronics} alt="" width={15} className="me-2" />
+        </button>
+        <button
+          type="button"
+          class="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF]">
+          <img src={toys} alt="" width={15} className="me-2" />
+        </button>
+        <button
+          type="button"
+          class="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF]">
+          <img src={sports} alt="" width={15} className="me-2" />
+        </button>
+        <button
+          type="button"
+          class="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF]">
+          <img src={outdoor} alt="" width={15} className="me-2" />
+        </button>
+      </nav> */}
     </div>
   );
 }
---
+++ src/component/example.js
@@ -0,0 +1,139 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./styles.css";
+
+import electronics from "../assets/electronics.svg";
+import sports from "../assets/sports.svg";
+import outdoor from "../assets/outdoor.svg";
+import toys from "../assets/toys.svg";
+import arrow1 from "../assets/arrow-1.svg";
+
+const initialButtonsData = [
+  {
+    label: "Electronics",
+    imageSrc: electronics,
+  },
+  {
+    label: "Toys and Games",
+    imageSrc: toys,
+  },
+  {
+    label: "Sports",
+    imageSrc: sports,
+  },
+  {
+    label: "Outdoor living",
+    imageSrc: outdoor,
+  },
+];
+
+const ButtonCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [buttonsData, setButtonsData] = useState(initialButtonsData);
+  const [buttonAddedFlags, setButtonAddedFlags] = useState(
+    Array(initialButtonsData.length).fill(false)
+  );
+
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    // Scroll to the end of the container when a new button is added
+    if (containerRef.current) {
+      containerRef.current.scrollLeft = containerRef.current.scrollWidth;
+    }
+  }, [buttonsData]);
+
+  const handleImageClick = () => {
+    const clickedImage = buttonsData[activeIndex].imageSrc;
+
+    // Check if the button for the clicked image has been added
+    if (!buttonAddedFlags[activeIndex]) {
+      // Add a new button with the same image to the initialButtonsData array
+      setButtonsData((prevButtonsData) => [
+        ...prevButtonsData,
+        { label: buttonsData[activeIndex].label, imageSrc: clickedImage },
+      ]);
+
+      setButtonAddedFlags((prevFlags) =>
+        prevFlags.map((flag, index) => (index === activeIndex ? true : flag))
+      );
+    }
+    setActiveIndex((prevIndex) => (prevIndex + 1) % initialButtonsData.length);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        gap: "40px",
+        margin: "auto",
+        maxWidth: "960px",
+        overflowX: "auto",
+        scrollbarWidth: "thin",
+        scrollbarColor: "transparent transparent",
+        // background: "black",
+        padding: "8px",
+        marginTop: "50px",
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      }}
+      ref={containerRef}>
+      <nav className="p-5 rounded-sm flex " style={{}}>
+        {buttonsData.map((button, index) => (
+          <button
+            key={index}
+            type="button"
+            style={{
+              width: "180px",
+              transition: "transform 0.3s ease-in-out",
+              transform: `translateX(${0}px) scaleY(${buttonAddedFlags[index] ? 1 : 0.8})`,
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+            }}
+            className="text-black h-10 bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF]">
+            <img src={button.imageSrc} alt="" width={15} className="me-2" />
+            {button.label}
+          </button>
+        ))}
+        {buttonAddedFlags[activeIndex] ? null : (
+          <img src={arrow1} alt="" width={25} className="me-2" />
+        )}
+        {buttonAddedFlags[activeIndex] ? null : (
+          <div
+            onClick={handleImageClick}
+            className="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF] cursor-pointer relative top-[-0.75rem]"
+            style={{
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+            }}>
+            <img
+              src={buttonsData[activeIndex].imageSrc}
+              alt=""
+              width={15}
+              className="me-1"
+            />
+          </div>
+        )}
+        {buttonAddedFlags[activeIndex ] ? null : (
+          <img src={arrow1} alt="" width={15} className="me-2" />
+        )}
+        {buttonAddedFlags[activeIndex+1] ? null : (
+          <div
+            // onClick={handleImageClick}
+            className="text-black bg-[#FFFFFF] hover:bg-[#FFFFFF] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center inline-flex items-center me-2 dark:bg-[#FFFFFF] dark:hover:bg-[#FFFFFF] relative top-[-1.75rem]"
+            style={{
+              boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+            }}>
+            <img
+              src={buttonsData[activeIndex + 1].imageSrc}
+              alt=""
+              width={10}
+              className="me-1"
+            />
+          </div>
+        )}
+      </nav>
+    </div>
+  );
+};
+
+export default ButtonCarousel;
